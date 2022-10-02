d5441cf97f6c5d7f4294eecee5d2ae80b14dc994
Antony <58532577+1tuz@users.noreply.github.com>
2022-10-02 23:00:05 +0300

    Update EasyAlias.sh

--- EasyAlias.sh
+++ EasyAlias.sh
@@ -1,4 +1,4 @@
-read -p "Введите алиас :" al
-read -p "Введите параметры :" par
+read -p "Enter the alias :" al
+read -p "Enter the parametrs :" par
 echo $al=\"$par\" >>/data/data/com.termux/files/home/.zshrc 2>/data/data/com.termux/files/home/myScripts/.trash
-echo "Алиас $al=\"$par\" успешно добавлен!"
+echo "Alias $al=\"$par\" was successfully added to .zshrc!"
