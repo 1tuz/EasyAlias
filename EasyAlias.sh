aec95d3663fe83b726e624d6f9c8ec8f7445ee26
Antony <58532577+1tuz@users.noreply.github.com>
2022-10-04 09:24:20 +0300

    Update EasyAlias.sh

--- EasyAlias.sh
+++ EasyAlias.sh
@@ -1,4 +1,4 @@
-read -p "Enter the new alias :" new
-read -p "Enter the old alias :" old
+read -p "Enter the new alias : " new
+read -p "Enter the pipe : " old
 echo alias $new=\"$old\" >>~/.zshrc 2>~/myScripts/.trash
 echo "Алиас $new=\"$old\" was successfully added to .zshrc!" && zsh && source ~/.zshrc>~/myScripts/.trash
