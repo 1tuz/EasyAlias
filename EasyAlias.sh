8781a8824a41b4afae34df9767509c2a932c1e63
Antony <58532577+1tuz@users.noreply.github.com>
2022-10-04 09:11:47 +0300

    Update EasyAlias.sh

--- EasyAlias.sh
+++ EasyAlias.sh
@@ -1,4 +1,4 @@
 read -p "Enter the new alias :" new
 read -p "Enter the old alias :" old
-echo $new=\"$old\" >>/data/data/com.termux/files/home/.zshrc 2>/data/data/com.termux/files/home/myScripts/.trash
-echo "Alias $new=\"$old\" was successfully added to .zshrc!"
+echo alias $new=\"$old\" >>~/.zshrc 2>~/myScripts/.trash
+echo "Алиас $new=\"$old\" was successfully added to .zshrc!" && zsh && source ~/.zshrc>~/myScripts/.trash
